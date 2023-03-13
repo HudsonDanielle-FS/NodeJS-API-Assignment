ace2f57cca1d5b620ff33f2b92cc5f51c141c217
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-03-13 18:40:02 -0500

    fixed random user router

--- router/randomUserRouter.js
+++ router/randomUserRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { randomUserService, randomUserById } = require("../services/randomUserService");
+const {
+  randomUserService,
+  randomUserById,
+} = require("../services/randomUserService");
 const randomUserRouter = express.Router();
 
 //localhost:3000/jokes/ten
@@ -18,7 +21,7 @@ randomUserRouter.get("/", (req, res, next) => {
 });
 
 //localhost:3000/jokes/23
-randomUserRouter.get("/:id", (req, res, next) => {
+randomUserRouter.get("/?gender=female", (req, res, next) => {
   randomUserServiceById(req.params.id)
     .then((result) => {
       res.status(200).json(result.data);
